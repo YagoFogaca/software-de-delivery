422130a23c2bf081ef12ff86b974000f1636a2aa
Yago Fogaça <yagofogaca20@gmail.com>
2022-06-23 15:53:08 -0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-console.log('Hello world')
+const prompt = require('prompt-sync')();
+console.clear();
+/*
 
-
-
-let nome = 'Yago'
+const pedido = {
+        hamburguer: 'X-Burguer',
+        ingredientes: [
+            'Pão',
+            '  Bife 90g',
+            '  Queijo',
+            '  Presunto',
+            '  Batata palha',
+            '  Alface',
+            '  Tomate',
+            '  Maionese caseira',
+            '  Ketchup',
+        ],
+        bebida: 'Coca-Cola',
+        obs: 'Bife mal passado',
+    };
+*/
